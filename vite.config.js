9d83beb2bd510ad713b7ef84cd5c3399478fe3d4
abii-07 <ABINESH.24@ichat.sp.edu.sg>
2026-04-28 23:31:07 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/nabeeha_midterms_gift/" : "/",
+  base: process.env.GITHUB_PAGES ? "/nabeehaaa_midterms_gift/" : "/",
   plugins: [react()]
 });
